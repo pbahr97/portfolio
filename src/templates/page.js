26c52f3defddd4991d06998cadb2374c342b0c71
Pirmin <bahr.pirmin@web.de>
2019-05-08 22:27:04 +0200

    wire up pages and post

---
+++ src/templates/page.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default ({ pageContext }) => {
+  return (
+    <div>
+      <h1>{pageContext.title}</h1>
+      <p>{pageContext.content}</p>
+    </div>
+  )
+}
